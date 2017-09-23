6ef7aae3fd3b3b22799ff3de839702b56fbd061f
Sumit Kumar <sumit.kumar@droom.in>
2017-09-24 00:06:25 +0530

    Week 5 Ex4 | Sigmoid Gradient

--- ex4/sigmoidGradient.m
+++ ex4/sigmoidGradient.m
@@ -12,22 +12,10 @@ g = zeros(size(z));
 % Instructions: Compute the gradient of the sigmoid function evaluated at
 %               each value of z (z can be a matrix, vector or scalar).
 
-
-
-
-
-
-
-
-
-
-
-
-
+g = sigmoid(z);
+vec1 = ones(size(z));
+g = g.*(vec1 - g);
 
 % =============================================================
-
-
-
 
 end
