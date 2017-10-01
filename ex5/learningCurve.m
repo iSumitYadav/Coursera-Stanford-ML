ad607c10452916e22f3ca5ee6d70435bc24f7186
Sumit Kumar <sumit.kumar@droom.in>
2017-10-01 20:18:30 +0530

    Week 6 Ex5 | Learning Curve

--- ex5/learningCurve.m
+++ ex5/learningCurve.m
@@ -53,11 +53,15 @@ error_val   = zeros(m, 1);
 
 % ---------------------- Sample Solution ----------------------
 
+for i = 1:m
+	xSubset = X(1:i, :);
+	ySubset = y(1:i);
 
+	theta = trainLinearReg(xSubset, ySubset, lambda);
 
-
-
-
+	error_train(i) = linearRegCostFunction(xSubset, ySubset, theta, 0);
+	error_val(i)   = linearRegCostFunction(Xval, yval, theta, 0);
+end
 
 % -------------------------------------------------------------
 
