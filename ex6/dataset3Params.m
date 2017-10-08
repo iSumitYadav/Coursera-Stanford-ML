56473c19cde5286b134996f87ee5dfa070ec33a7
Sumit Kumar <sumit.kumar@droom.in>
2017-10-08 13:10:38 +0530

    Week 7 Ex6 | Parameters (C, sigma) for Dataset 3

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -23,11 +23,27 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+res = eye(64, 3);
+errRow = 0;
 
+for C = [0.01 0.03 0.1 0.3 1 3 10 30]
+	for sigma = [0.01 0.03 0.1 0.3 1 3 10 30]
+		model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+		predictions = svmPredict(model, Xval);
+		predictionError = mean(double(predictions ~= yval));
 
+		% store C in col 1, sigma in col 2, and predictionError in 3
+		errRow += 1;
+		res(errRow, :) = [C, sigma, predictionError];
+	end
+end
 
+% Final result is sorted based on min error first
+sortedResult = sortrows(res, 3);
 
-
+% Return the first row C and sigma as they are having the min error
+C = sortedResult(1, 1);
+sigma = sortedResult(1, 2);
 
 % =========================================================================
 
