ef5015b9fbd25b518952ebf41d0d20223fb36574
Sumit Kumar <sumit.kumar@droom.in>
2017-10-08 13:50:33 +0530

    Week 7 Ex 6 | Email Preprocessing

--- ex6/processEmail.m
+++ ex6/processEmail.m
@@ -97,14 +97,11 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
 
+    index = strmatch(str, vocabList, 'exact');
 
-
-
-
-
-
-
-
+    if index
+        word_indices = [word_indices; index];
+    end
 
     % =============================================================
 
